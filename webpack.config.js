34b42eced0b5bff079f4e43f60cfb8ab0b0591f7
Maksim Ryzhikov <rv.maksim@gmail.com>
2016-02-19 17:40:33 +0300

    chore: build throught webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  debug: true,
+  devtool: 'source-map',
+  entry: {
+    'index.android': ['./index.android.js']
+  },
+  output: {
+    path: require('path').resolve(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx|es6)$/,
+      loader: 'babel',
+      query: {
+        cacheDirectory: true,
+        presets: ['es2015', 'stage-1', 'react']
+      }
+    }]
+  }
+};
